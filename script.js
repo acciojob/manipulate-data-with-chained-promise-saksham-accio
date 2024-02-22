0c9941a8a32cfa601d0169893f55070cd6ccfb25
saksham-accio <125554544+saksham-accio@users.noreply.github.com>
2024-02-22 20:06:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+const outputInput = document.getElementById("output")
+
+let array = [1, 2, 3, 4];
+
+function newManipulation(array) {
+    return new Promise((res, rej) => {
+        setTimeout(()=> {
+            res (array)
+        } , 3000)
+    }).then(array => {
+        return new Promise ((res, rej) => {
+            setTimeout(() => {
+                const evenNumber =array.filter((num) => num % 2 == 0 )
+                outputInput.innerText= evenNumber.join(', ')
+                res (evenNumber);
+            }, 1000);
+        })
+    }).then(evenNumber => {
+        return new Promise ((res, rej)=> {
+            setTimeout(()=> {
+                const evenMulti = evenNumber.map((num) => num * 2)
+                outputInput.innerText= evenMulti.join(', ')
+                res(evenMulti);
+            }, 2000)
+        })
+    }).catch(error => {
+        console.error('Error', error)
+    })
+}
+
+newManipulation(array)

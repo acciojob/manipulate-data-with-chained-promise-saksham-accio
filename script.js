aca5ea9826a5a45342f6cb9c108b9b0bbc5eb2f6
saksham-accio <125554544+saksham-accio@users.noreply.github.com>
2024-02-22 20:14:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,24 +3,25 @@ const outputInput = document.getElementById("output")
 let array = [1, 2, 3, 4];
 
 function newManipulation(array) {
-        return new Promise ((res, rej) => {
-            setTimeout(() => {
-                const evenNumber =array.filter((num) => num % 2 == 0 )
-                outputInput.innerText= evenNumber.join(', ')
-                res (evenNumber);
-            }, 1000);
-        })
-    }).then(evenNumber => {
-        return new Promise ((res, rej)=> {
-            setTimeout(()=> {
-                const evenMulti = evenNumber.map((num) => num * 2)
-                outputInput.innerText= evenMulti.join(', ')
-                res(evenMulti);
-            }, 2000)
-        })
-    }).catch(error => {
-        console.error('Error', error)
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const evenNumber = array.filter((num) => num % 2 == 0);
+      outputInput.innerText= evenNumber.join(', ')
+      res(evenNumber);
+    }, 1000);
+  })
+    .then((evenNumber) => {
+      return new Promise((res, rej) => {
+        setTimeout(() => {
+          const evenMulti = evenNumber.map((num) => num * 2);
+          outputInput.innerText= evenMulti.join(', ')
+          res(evenMulti);
+        }, 2000);
+      });
     })
+    .catch((error) => {
+      console.error("Error", error);
+    });
 }
 
 newManipulation(array)
